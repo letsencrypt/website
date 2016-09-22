492b312c0090255c73c4395719d0695ff66ba6ce
J.C. Jones <jc@mozilla.com>
2016-09-22 17:04:37 +0100

    Tighten CSP
    Move Google Analytics to its own file and load it per guidance:
    
    https://developers.google.com/analytics/devguides/collection/analyticsjs/#alternative_async_tracking_snippet
    
    This in combination with PR #61 permits the following, much tighter CSP
    policy:
    
    > Content-Security-Policy "default-src 'self'; style-src 'unsafe-inline' 'self'; script-src 'unsafe-eval' 'self' https://www.google-analytics.com; img-src 'self' data: https://www.google-analytics.com;"
    
    This can land whenever, but the CSP policy shouldn't be updated until #61 lands.
    Additionally, the CSP policy should be updated with care and tested as well as
    possible before being left alone.

---
+++ js/ga.js
@@ -0,0 +1,5 @@
+// Google Analytics startup, per:
+// https://developers.google.com/analytics/devguides/collection/analyticsjs/#alternative_async_tracking_snippet
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-56433935-1', 'auto');
+ga('send', 'pageview');
