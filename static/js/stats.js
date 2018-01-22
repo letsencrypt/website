ace5f1ceffd1dd98a41fd7df9816a493b4498bf1
J.C. Jones <jc@mozilla.com>
2018-01-22 16:38:37 -0700

    Redesign HTTPS statistics to use new Firefox Telemetry
    Mozilla in Firefox 58 has removed API access to release telemetry for many
    probes, including HTTP_PAGELOAD_IS_SSL, but has provided a dimensional dataset
    via SQL [1][2].
    
    This patch changes acquisition of the HTTPS adoption data into two parts:
    
     1. The historical data, which has been shown here for Let's Encrypt for some
        time. This file was cut at the date where the new data begins, 2016-11-01.
        Note that this is the same data as
        https://ct.tacticalsecret.com/secure-pageloads.tsv but trimmed and converted
        to CSV.
    
     2. The current data, which is generated ~daily by Mozilla. Currently this
        dataset is not published by Mozilla on a Mozilla property (being remedied),
        but for now it can be downloaded from
        https://ct.tacticalsecret.com/current-https-adoption.csv . Operations will
        need to configure the web server to download this file at a regular
        occurrence and overwrite the one being checked in as part of this commit.
    
    These two parts are stitched together by the `httpsPlot()` method, which is
    now called via a `Promise.all()` to ensure that the two dependent fetches
    both complete before we start to plot the data.
    
    The current dataset has dimensionality for the adoption figures broken out by
    operating system and country. (Note: Country is by geo-ip of the submission, not
    localization.)
    
    The graphing code has provisions within it to graph any combination of country
    and operating system (see the list in `httpsPlot()`). It should be
    straightforward to modify this code to add in a user interface to plot custom
    graphs. `gHttpsData.countryList` and `gHttpsData.osList` are populated with
    the available data for each.
    
    Note: Not all dimensions will have data for each day. If there were too few
    pings for a given OS / Country combination, there will not be data in the
    dataset. This is not a bug, it is a deliberate outcome of Firefox Telemetry's
    privacy policy.
    
    Since this uses Promises and Fetch, it is incompatible with Internet Explorer.
    That is probably not a concern?
    
    [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1414839
    [2] https://github.com/mozilla/firefox-data-docs/pull/80

--- static/js/stats.js
+++ static/js/stats.js
@@ -2,22 +2,26 @@
 // and then display it using plotly.js.
 
 var numFormat = /\d+/;
+var dateFormat = /\d{4}-\d{2}-\d{2}/;
 
-var history_cutover = {
+var gHttpsData = { countryList: [], osList: [], dateToCountryOSPageloadData: {},
+                   dateToHistoricalPageloadData: {} };
+
+var gHistoryCutover = {
   x: "2017-07-03", y: 33926513, xref: "x", yref: "y",
   text: "Methodology Change", showarrow: true, arrowhead: 7, ax: -40, ay: 40
 };
 
 // Process a string `s` and, for each row (line), call `f()` with an
 // array of each tab-separated value within that row.
-function parse_tsv(s, f) {
+function parse_delim(s, delim, f) {
   var ix_end = 0;
   for (var ix = 0; ix < s.length; ix = ix_end + 1) {
     ix_end = s.indexOf('\n', ix);
     if (ix_end == -1) {
       ix_end = s.length;
     }
-    var row = s.substring(ix, ix_end).split('\t');
+    var row = s.substring(ix, ix_end).trim().split(delim);
     f(row);
   }
 }
@@ -63,11 +67,9 @@ function tsvListener() {
   var tPctTLSAvg = { type: "scatter", name: "% of Firefox Pageloads use TLS (14 day moving average)",
                      x:[], y:[] }
 
-  var dateFormat = /\d{4}-\d{2}-\d{2}/;
-
   var stackPctTLSAvg = [];
 
-  parse_tsv(this.responseText, function(row){
+  parse_delim(this.responseText, '\t', function(row){
     if (!dateFormat.test(row[0])) {
       return;
     }
@@ -102,7 +104,7 @@ function plot(tIssued, tActive, tFqdn, tRegDom, tPctTLSAvg) {
         x: 0,
         y: 1
       },
-      annotations: [ history_cutover ]
+      annotations: [ gHistoryCutover ]
     }
     activeUsage = document.getElementById('activeUsage');
     if (activeUsage) {
@@ -131,29 +133,6 @@ function plot(tIssued, tActive, tFqdn, tRegDom, tPctTLSAvg) {
     }
   }
 
-  // Firefox telemetry (HTTP_PAGELOAD_IS_SSL) over time
-  {
-    traces = [ tPctTLSAvg ];
-    layout = {
-      margin: { t: 20 },
-      yaxis: {
-        title: 'Percent of Pageloads over HTTPS',
-        rangemode: 'tozero',
-        ticksuffix: '%'
-      },
-      legend: {
-        xanchor: "left",
-        yanchor: "top",
-        x: 0,
-        y: 1
-      }
-    }
-    pageloadPercent = document.getElementById('pageloadPercent');
-    if (pageloadPercent) {
-      Plotly.plot(pageloadPercent, traces, layout);
-    }
-  }
-
   // Combined Graph: issuancePerDay + activeUsage
   {
     // Override the axis for the combined graph
@@ -179,7 +158,7 @@ function plot(tIssued, tActive, tFqdn, tRegDom, tPctTLSAvg) {
         x: 0,
         y: 1
       },
-      annotations: [ history_cutover ]
+      annotations: [ gHistoryCutover ]
     }
     combinedTimeline = document.getElementById('combinedTimeline');
     if (combinedTimeline) {
@@ -188,7 +167,209 @@ function plot(tIssued, tActive, tFqdn, tRegDom, tPctTLSAvg) {
   }
 }
 
+function httpsCsvListener(responseText) {
+  let countries = {};
+  let operatingSystems = {};
+  let dateToCountryOSPageloadData = {};
+
+  parse_delim(responseText, ',', function(row){
+    let datestamp = row[0].substring(0, 10); // Strip off the timestamp
+    let os = row[1];
+    let country = row[2];
+    let reporting_ratio = row[3];
+    let normalized_pageloads = row[4];
+    let ratio = row[5];
+    if (!dateFormat.test(datestamp)) {
+      return;
+    }
+
+    if (!(datestamp in dateToCountryOSPageloadData)) {
+      dateToCountryOSPageloadData[datestamp] = {};
+    }
+
+    let countryOSPageloadData = dateToCountryOSPageloadData[datestamp];
+    if (!(country in countryOSPageloadData)) {
+      countryOSPageloadData[country] = {};
+    }
+
+    let osPageloadData = countryOSPageloadData[country];
+    osPageloadData[os] = { normalized_pageloads, reporting_ratio, ratio };
+
+    // derive a master list for the UI
+    countries[country] = 1;
+    operatingSystems[os] = 1;
+  });
+
+  gHttpsData.countryList = Object.keys(countries);
+  gHttpsData.osList = Object.keys(operatingSystems);
+  gHttpsData.dateToCountryOSPageloadData = dateToCountryOSPageloadData;
+}
+
+function httpsDerivePageloadsFromNormalizedData(traceObj, includeInHttpsAnalysis,
+                                                stackMovingAverage = []) {
+  let dateToCountryOSPageloadData = gHttpsData.dateToCountryOSPageloadData;
+
+  // Input data is not sorted, and so neither is the dictionary, so we need
+  // to sort it before we continue.
+  let sortedDates = Object.keys(dateToCountryOSPageloadData);
+  sortedDates.sort();
+  for (let datestamp of sortedDates) {
+    // See https://bugzilla.mozilla.org/show_bug.cgi?id=1414839#c19 for
+    // parameter details
+    let totalNormalizedPageloads = 0.0;
+
+    // Preprocess to sum all normalized_pageloads for included rows
+    for (let country in dateToCountryOSPageloadData[datestamp]) {
+      for (let os in dateToCountryOSPageloadData[datestamp][country]) {
+        if (includeInHttpsAnalysis(os, country)) {
+          let row = dateToCountryOSPageloadData[datestamp][country][os];
+          totalNormalizedPageloads += parseFloat(row.normalized_pageloads);
+        }
+      }
+    }
+
+    // Now derive the reporting ratio and secure pageload ratio
+    let totalReportingRatio = 0.0;
+    let totalSecurePageloadRatio = 0.0;
+
+    for (let country in dateToCountryOSPageloadData[datestamp]) {
+      for (let os in dateToCountryOSPageloadData[datestamp][country]) {
+        if (includeInHttpsAnalysis(os, country)) {
+          let row = dateToCountryOSPageloadData[datestamp][country][os];
+          let dimensionNormalizedPageloads = parseFloat(row.normalized_pageloads)
+                                               / totalNormalizedPageloads;
+          totalReportingRatio += parseFloat(row.reporting_ratio)
+                                   * dimensionNormalizedPageloads;
+          totalSecurePageloadRatio += parseFloat(row.ratio)
+                                         * dimensionNormalizedPageloads;
+        }
+      }
+    }
+
+    let securePageloadPercentage = movingAvg(stackMovingAverage,
+                                             totalSecurePageloadRatio * 100);
+    insertPoint(traceObj, datestamp, securePageloadPercentage);
+  }
+}
+
+function historicalHttpsCsvListener(responseText) {
+  parse_delim(responseText, ',', function(row){
+    let datestamp = row[0];
+    let pageloads = row[1];
+    if (!dateFormat.test(datestamp)) {
+      return;
+    }
+
+    gHttpsData.dateToHistoricalPageloadData[datestamp] = pageloads;
+  });
+}
+
+function importHistoricalGlobalData(traceObj, stackMovingAvg) {
+  let sortedDates = Object.keys(gHttpsData.dateToHistoricalPageloadData);
+  sortedDates.sort();
+  for (let datestamp of sortedDates) {
+    let pageloadPercent = gHttpsData.dateToHistoricalPageloadData[datestamp];
+    let securePageloadPercentage = movingAvg(stackMovingAvg,
+                                             pageloadPercent);
+    insertPoint(traceObj, datestamp, securePageloadPercentage);
+  }
+}
+
+// Firefox telemetry (HTTP_PAGELOAD_IS_SSL) over time
+function httpsPlot() {
+  let traces = [];
+
+  {
+    let traceObj = { type: "scatter", x:[], y:[], name: "Global users" }
+    let stackMovingAvg = [];
+    importHistoricalGlobalData(traceObj, stackMovingAvg);
+    httpsDerivePageloadsFromNormalizedData(traceObj, (os, country) => {
+      return true;
+    }, stackMovingAvg);
+    traces.push(traceObj);
+  }
+  {
+    let traceObj = { type: "scatter", x:[], y:[], name: "Germany users" }
+    httpsDerivePageloadsFromNormalizedData(traceObj, (os, country) => {
+      return (country == "DE");
+    });
+    traces.push(traceObj);
+  }
+  {
+    let traceObj = { type: "scatter", x:[], y:[], name: "USA users" }
+    httpsDerivePageloadsFromNormalizedData(traceObj, (os, country) => {
+      return (country == "US");
+    });
+    traces.push(traceObj);
+  }
+  {
+    let traceObj = { type: "scatter", x:[], y:[], name: "OSX users" }
+    httpsDerivePageloadsFromNormalizedData(traceObj, (os, country) => {
+      return (os == "Darwin");
+    });
+    traces.push(traceObj);
+  }
+  {
+    let traceObj = { type: "scatter", x:[], y:[], name: "Windows users" }
+    httpsDerivePageloadsFromNormalizedData(traceObj, (os, country) => {
+      return (os == "Windows_NT");
+    });
+    traces.push(traceObj);
+  }
+  {
+    let traceObj = { type: "scatter", x:[], y:[], name: "Windows users in France" }
+    httpsDerivePageloadsFromNormalizedData(traceObj, (os, country) => {
+      return (os == "Windows_NT" && country == "FR");
+    });
+    traces.push(traceObj);
+  }
+
+  layout = {
+    margin: { t: 20 },
+    yaxis: {
+      title: 'Percent of Pageloads over HTTPS (14 day moving average)',
+      rangemode: 'tozero',
+      ticksuffix: '%'
+    },
+    legend: {
+      xanchor: "left",
+      yanchor: "top",
+      x: 0,
+      y: 1
+    }
+  }
+  pageloadPercent = document.getElementById('pageloadPercent');
+  if (pageloadPercent) {
+    Plotly.plot(pageloadPercent, traces, layout);
+  }
+}
+
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", tsvListener);
 oReq.open("GET", "/js/cert-timeline.tsv");
 oReq.send();
+
+var currentHttpsReqPromise = fetch("/js/current-https-adoption.csv")
+.then((response) => {
+  return response.text();
+}).then((text) => {
+  httpsCsvListener(text);
+});
+
+var historicalHttpsReqPromise = fetch("/js/historical-https-adoption.csv")
+.then((response) => {
+  return response.text();
+}).then((text) => {
+  historicalHttpsCsvListener(text);
+});
+
+// We shouldn't try to plot HTTPS until both the current and historical fetches
+// are completed
+Promise.all([currentHttpsReqPromise, historicalHttpsReqPromise])
+.then(() => {
+  if (document.readyState === "complete") {
+    httpsPlot();
+  } else {
+    window.addEventListener("load", httpsPlot);
+  }
+});
