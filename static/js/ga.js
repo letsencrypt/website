89520c7fd60b0ff2dc5be3f2eb9801ce2368aa08
Josh Aas <jaas@kflag.net>
2018-03-26 18:02:02 -0500

    Convert Google Analytics to tag manager, add back csp-compliant conversion tracking.

--- static/js/ga.js
+++ static/js/ga.js
@@ -1,5 +1,24 @@
-// Google Analytics startup, per:
-// https://developers.google.com/analytics/devguides/collection/analyticsjs/#alternative_async_tracking_snippet
-window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-ga('create', 'UA-56433935-1', 'auto');
-ga('send', 'pageview');
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-56433935-1');
+
+// AdWords Conversion Tracking
+gtag('config', 'AW-872454614');
+function gtag_report_conversion(url) {
+  var callback = function () {
+    if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
+  };
+  gtag('event', 'conversion', {
+      'send_to': 'AW-872454614/r2JLCP7BiGwQ1rOCoAM',
+      'event_callback': callback
+  });
+  return false;
+}
+var el = document.getElementById("getting-started-button");
+if (el) {
+  el.addEventListener("click", function(){gtag_report_conversion("https://letsencrypt.org/getting-started/")}, false);
+}
