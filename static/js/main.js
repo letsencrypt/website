44b9c570355a783b9fc0aae33eef2dd66eb42a46
Sara Bee <sara@turbinelabs.io>
2018-10-16 16:44:56 -0400

    Fix dropdown navigation on small screens

--- static/js/main.js
+++ static/js/main.js
@@ -3,12 +3,38 @@
 //==============
 
 (function (window, document) {
-var menu = document.getElementById('menu'),
-    WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
+
+const MENU_ID = 'menu';
+const MENU_SELECTOR = `#${MENU_ID}`;
+
+const MENU_OPEN_CLASS = 'open';
+
+// Applied to NAV_MENU_PARENTs when they're active/open.
+// Note that this doesn't not get applied with NAV_MENU_HOVERABLE.
+const NAV_MENU_ACTIVE_CLASS = 'pure-menu-active';
+const NAV_MENU_ACTIVE_SELECTOR = `.${NAV_MENU_ACTIVE_CLASS}`;
+
+// When applied to a NAV_MENU_PARENT_ item, the menu will toggle on hover
+// (See https://purecss.io/menus/#dropdowns)
+const NAV_MENU_HOVERABLE_CLASS = 'pure-menu-allow-hover';
+
+// Applied to nav menu items that are parents of a sub-menu
+const NAV_MENU_PARENT_CLASS = 'pure-menu-has-children';
+const NAV_MENU_PARENT_SELECTOR = `.${NAV_MENU_PARENT_CLASS}`;
+
+// The containing <ul> of a sub-menu's items
+const NAV_MENU_CHILD_CLASS = 'pure-menu-children';
+const NAV_MENU_CHILD_SELECTOR = `.${NAV_MENU_CHILD_CLASS}`;
+
+const NAV_LINK_CLASS = 'pure-menu-link';
+const NAV_LINK_SELECTOR = `.${NAV_LINK_CLASS}`;
+
+const menu = document.getElementById(MENU_ID);
+const WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
 
 function toggleHorizontal() {
     [].forEach.call(
-        document.getElementById('menu').querySelectorAll('.custom-can-transform'),
+        document.getElementById(MENU_ID).querySelectorAll('.custom-can-transform'),
         function(el){
             el.classList.toggle('pure-menu-horizontal');
         }
@@ -26,116 +52,106 @@ function toggleMenu() {
         toggleHorizontal();
         disableDropdowns();
     }
-    menu.classList.toggle('open');
+    menu.classList.toggle(MENU_OPEN_CLASS);
     // document.getElementById('menuIcon').classList.toggle('x');
 }
 
-function closeMenu() {
-    if (menu.classList.contains('open')) {
-        toggleMenu();
-    }
+function openNavMenu(el) {
+  closeAllNavMenus(el);
+  el.classList.add(NAV_MENU_ACTIVE_CLASS);
+  el.querySelector(`${NAV_MENU_PARENT_SELECTOR} > ${NAV_LINK_SELECTOR}`)
+    .setAttribute('aria-expanded', true);
 }
 
-var dropdownListener = function(el) {
-  el.addEventListener('click', function (e) {
-    el.classList.toggle('isOpen');
-    el.querySelector('.pure-menu-children').classList.toggle('show-children');
-  });
-};
+function closeNavMenu(el) {
+  el.classList.remove(NAV_MENU_ACTIVE_CLASS);
+  el.querySelector(`${NAV_MENU_PARENT_SELECTOR} > ${NAV_LINK_SELECTOR}`)
+    .setAttribute('aria-expanded', false);
+}
+
+function closeAllNavMenus(exclude) {
+  [].forEach.call(
+    menu.querySelectorAll(NAV_MENU_ACTIVE_SELECTOR), function(el) {
+      if (el !== exclude) {
+        closeNavMenu(el);
+      }
+    }
+  );
+}
 
 function enableDropdowns() {
   [].forEach.call(
-      document.getElementById('menu').querySelectorAll('.pure-menu-has-children'),
+      document.getElementById(MENU_ID).querySelectorAll(NAV_MENU_PARENT_SELECTOR),
       function(el){
-          dropdownListener(el);
-          el.classList.toggle('pure-menu-allow-hover');
+        el.classList.add(NAV_MENU_HOVERABLE_CLASS);
+        el.addEventListener('focusin', function(e) {
+          openNavMenu(el);
+        })
       }
   );
 }
 
 function disableDropdowns() {
   [].forEach.call(
-      document.getElementById('menu').querySelectorAll('.pure-menu-has-children'),
+      document.getElementById(MENU_ID).querySelectorAll(NAV_MENU_PARENT_SELECTOR),
       function(el){
-          el.removeEventListener('click', dropdownListener(el));
-          el.classList.toggle('pure-menu-allow-hover');
+        el.classList.remove(NAV_MENU_HOVERABLE_CLASS);
       }
   );
 }
 
-document.getElementById('menuIcon').addEventListener('click', function (e) {
+function closeMenu() {
+  if (menu.classList.contains(MENU_OPEN_CLASS)) {
+    closeAllNavMenus();
     toggleMenu();
+  }
+}
+
+document.getElementById('menuIcon').addEventListener('click', function (e) {
+  // Don't propagate to the document-level click listener, since that closes the menu
+  e.stopPropagation();
+  toggleMenu();
 });
 
 [].forEach.call(
-  document.getElementById('menu').querySelectorAll('.pure-menu-has-children'), function(el) {
+  document.getElementById(MENU_ID).querySelectorAll(NAV_MENU_PARENT_SELECTOR), function(el) {
     el.firstElementChild.addEventListener('click', function (e) {e.preventDefault();});
   }
 );
 
 window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
 
-
-function closeNavMenus(exclude) {
-  [].forEach.call(
-    menu.querySelectorAll('.pure-menu-active'), function(el) {
-      if (el !== exclude) {
-        el.classList.remove('pure-menu-active');
-        el.querySelector('.pure-menu-has-children > .pure-menu-link').setAttribute('aria-expanded', false);
-      }
-    }
-  );
-}
-
 // Initialize nav menu aria roles/state
 menu.querySelector('.pure-menu-list').setAttribute('role', 'menubar');
 
 [].forEach.call(
-  menu.querySelectorAll('.pure-menu-link'), function(el) {
+  menu.querySelectorAll(NAV_LINK_SELECTOR), function(el) {
     el.setAttribute('role', 'menuitem');
     el.parentNode.setAttribute('role', 'none');
 
-    if (el.parentNode.classList.contains('pure-menu-has-children')) {
+    if (el.parentNode.classList.contains(NAV_MENU_PARENT_CLASS)) {
       el.setAttribute('aria-haspopup', true);
       el.setAttribute('aria-expanded', false);
 
-      var childMenu = el.parentNode.querySelector('.pure-menu-children');
+      var childMenu = el.parentNode.querySelector(NAV_MENU_CHILD_SELECTOR);
       childMenu.setAttribute('role', 'menu');
       childMenu.setAttribute('aria-label', el.text);
     }
   }
 );
 
-menu.addEventListener('focusin', function(e) {
-  if (e.target.classList.contains('pure-menu-link')) {
-    var anchor = e.target;
+enableDropdowns();
 
-    var listItem = anchor.parentNode;
-    if (listItem.parentNode.classList.contains('pure-menu-children'))
-      listItem = listItem.parentNode.parentNode;
-
-    var listItemAnchor = listItem.querySelector('.pure-menu-link');
-
-    closeNavMenus(listItem);
-
-    if (listItem.classList.contains('pure-menu-has-children')) {
-      listItem.classList.add('pure-menu-active');
-      listItemAnchor.setAttribute('aria-expanded', true);
-    }
+document.addEventListener('click', function(e) {
+  if (!e.target.closest(MENU_SELECTOR)) {
+    closeMenu();
   }
-});
-
-
-document.addEventListener('click', closeNavMenus);
-
-document.addEventListener('focusin', function(e) {
-  if (!e.target.classList.contains('pure-menu-link'))
-    closeNavMenus();
-});
+})
 
 document.addEventListener('keyup', function(e) {
-  if (e.keyCode == 27 && e.target.classList.contains('pure-menu-link'))
-    closeNavMenus();
-});
+  if (e.keyCode === 27) {
+    closeMenu();
+  }
+})
 
 })(this, this.document);
