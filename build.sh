475e1e58c884b76cba9494177467415359eda84e
Josh Aas <jaas@kflag.net>
2018-10-20 15:55:31 -0400

    Use uglify-es instead of uglify-js because we need es6 support. Also add some status output to build script.

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 # This script uses node-sass and uglifyjs
 # npm install -g node-sass
-# npm install -g uglify-js
+# npm install -g uglify-es
 
+echo "Processing scss..."
 node-sass --output-style compressed ./src/css/main.scss ./static/css/main.min.css
-
+echo "Minifying js..."
 uglifyjs ./static/js/main.js -o ./static/js/main.min.js  -c -m
