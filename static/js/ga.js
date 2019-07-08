32128ec0a0c2fe99dec5eeb72a59cfa49961e3c5
Tom Delmas <github@tdelmas.eu>
2019-07-08 09:35:37 +0000

    Defer loading of Google Analytics and enable IP anonymization (#568)
    There are no reasonable needs to slow the page loading for analytics purpose.
    Enabling IP anonymization is better for privacy and have negligible drawbacks.
    (If IP anonymization is already enabled in the Google Analytics console, that will just confirm the pledge to respect users privacy)
    Cf. #107 and #324
    Cf. https://developers.google.com/analytics/devguides/collection/gtagjs/ip-anonymization and https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#aip

--- static/js/ga.js
+++ static/js/ga.js
@@ -4,4 +4,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 /*eslint-enable*/
 gtag("js", new Date());
-gtag("config", "UA-56433935-1");
+gtag("config", "UA-56433935-1", { 'anonymize_ip': true });
