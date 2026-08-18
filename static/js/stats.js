14ed868ad9d30a6f2bb8b6fd716f2f5d33a04296
lenaunderwood22 <46858616+lenaunderwood22@users.noreply.github.com>
2026-08-18 13:15:43 -0700

    Revert "Update website to read stats from new distribution (#2286)"
    This reverts commit a33b4fca8065085dfad8e0b6bf680fea7f4638d5.

--- static/js/stats.js
+++ static/js/stats.js
@@ -319,20 +319,13 @@ function doPlot() {
   }
 
   var path;
-  if ( location.hostname === "localhost" ) {
-    path = "/js/"; // in dev, will use old data.
-  } else {
+  if ( location.hostname === "letsencrypt.org" ) {
     path = "https://d4twhgtvn0ff5.cloudfront.net/";
-  }
-
-  var statsPath;
-  if ( location.hostname === "localhost" ) {
-    statsPath = "/js/"; // in dev, will use old data.
   } else {
-    statsPath = "https://d1dfn7jg27m4cf.cloudfront.net/";
+    path = "/js/"; // in dev, will use old data.
   }
 
-  fetch(statsPath+"cert-daily-stats.tsv")
+  fetch(path+"cert-timeline.tsv")
   .then(response => {
     return response.text();
   }).then(tsvListener);
